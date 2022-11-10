9e43f6a8094eb3f21deee1940891268980220ec2
White Buffalo <apothocare@gmail.com>
2022-11-10 13:22:39 -0600

    Initial User CRUD routes added to controller folder

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,18 +1,61 @@
 const {User} = require('../Models/User');
 
 const userController = {
-   
     //get all users
+    getAllUsers(req, res) {
+        User.find({})
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
 
     //get single user by _id and populated thought and friend data
-
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No User found with this id.'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
     //post a new user
-
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
     //put to update a user by its _id
-
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({_id: params.id }, body, {new: true})
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id.'});
+                    return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
     //delete to remove user  by its _id
-
-
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id})
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No User found with this id.'});
+                return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
+    }
 };
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,10 @@
 const router = require('express').Router();
+const {getAllUsers} = require('../../controllers/user-controller');
+
+//Set up GET all and POST at /api/users
+router
+    .route('/')
+    .get(getAllUsers);
+
+
+module.exports = router;
